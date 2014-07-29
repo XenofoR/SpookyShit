909e35b71ed5bf666485f5938715d4ae15d96637
Kim Hansson <tec_shadow@hotmail.com>
2014-07-29 14:38:29 +0200

    WIP
    Working on simple uniform Translation on trigger

---
+++ Nykr/Assets/ZoneScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider p_other)
+	{
+		if (p_other.tag == "Player") 
+		{
+			p_other.transform.Translate(200, 0, 0, Space.Self);
+		}
+	}
+}
