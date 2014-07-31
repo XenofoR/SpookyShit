63954a1e7cadafb66a8880b69edcf9c7cf0c810b
Kim Hansson <tec_shadow@hotmail.com>
2014-07-31 11:06:32 +0200

    Fixed issue with the linear warp

--- Nykr/Assets/ZoneScript.cs
+++ Nykr/Assets/ZoneScript.cs
@@ -7,12 +7,14 @@ public class ZoneScript : MonoBehaviour {
 	bool m_pointMove;
 	int m_Id;
 	GameObject[] m_points;
+	PlayerScript m_playerScript;
 	// Use this for initialization
 	void Start () 
 	{
 		m_linearMove = true;
 		m_pointMove = false;
-
+	
+		m_playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
 		m_Id = int.Parse(gameObject.tag[4].ToString());
 
 		m_points = GameObject.FindGameObjectsWithTag("WarpPoint");
@@ -40,17 +42,24 @@ public class ZoneScript : MonoBehaviour {
 
 	void OnTriggerEnter(Collider p_other)
 	{
-		if(p_other.tag == "Player")
+		if(p_other.tag == "Player" && !m_playerScript.JustWarped())
 		{
 			Terrain terra = Terrain.activeTerrain;
 			Vector3 terraSize = terra.terrainData.size;
-			float moveOffset = terraSize.x - p_other.transform.localPosition.x; //Offset the current camera position so that we move the length of the terrain minus what we have walked
 			if(m_linearMove)
-				p_other.transform.Translate(-(terraSize.x - moveOffset), 0, 0, gameObject.transform); //Space.World so that we do not use the camera direction
+				p_other.transform.Translate(terraSize.x - gameObject.renderer.bounds.size.x, 0, 0, gameObject.transform); //Space.World so that we do not use the camera direction
 			else if(m_pointMove)
 			{
 				p_other.transform.localPosition = m_points[0].transform.localPosition;
 			}
+
+			m_playerScript.SetPlayerWarp(true);
 		}
+	}
+
+	void OnTriggerExit(Collider p_other)
+	{
+		if (p_other.tag == "Player")
+			m_playerScript.SetPlayerWarp(false);
 	}
 }
