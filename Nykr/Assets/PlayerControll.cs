8d377f116130e637d0911593482f6e818e3f5e33
Kim Hansson <tec_shadow@hotmail.com>
2015-03-12 21:28:35 +0100

    fixed component fetch and amount of force added each update

--- Nykr/Assets/PlayerControll.cs
+++ Nykr/Assets/PlayerControll.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class PlayerControll : MonoBehaviour 
 {
 	Dictionary<string, KeyCode> m_keys;
+	GameObject m_player;
 	float m_normalSpeed;
 	float m_sprintSpeed;
 	float m_sprintLength;
@@ -12,6 +13,7 @@ public class PlayerControll : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
+		m_player = GameObject.FindGameObjectWithTag("Player");
 		m_keys = new Dictionary<string, KeyCode>();
 		m_keys.Add ("forward", KeyCode.W);
 		m_keys.Add ("backward", KeyCode.S);
@@ -22,8 +24,8 @@ public class PlayerControll : MonoBehaviour
 		m_keys.Add ("sprint", KeyCode.LeftShift);
 		m_keys.Add ("crouch", KeyCode.C);
 
-		m_normalSpeed = 10;
-		m_sprintSpeed = 15;
+		m_normalSpeed = 500;
+		m_sprintSpeed = 750;
 		m_sprintLength = 0;
 		m_movement = new Vector2(0,0);
 	}
@@ -36,18 +38,25 @@ public class PlayerControll : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		m_movement.x = 0;
-		m_movement.y = 0;
 
 		if(Input.GetKeyDown(m_keys["forward"]))
-			m_movement.x = 1;
-		if(Input.GetKeyDown(m_keys["backward"]))
-			m_movement.x = -1;
-		if(Input.GetKeyDown(m_keys["strafe_left"]))
 			m_movement.y = 1;
-		if(Input.GetKeyDown(m_keys["strafe_right"]))
+		if(Input.GetKeyDown(m_keys["backward"]))
 			m_movement.y = -1;
+		if(Input.GetKeyDown(m_keys["strafe_left"]))
+			m_movement.x = -1;
+		if(Input.GetKeyDown(m_keys["strafe_right"]))
+			m_movement.x = 1;
 
-		GetComponent<Rigidbody>().AddForce (new Vector3 (m_movement.x * m_normalSpeed, 0, m_movement.y * m_normalSpeed));
+		if(Input.GetKeyUp(m_keys["forward"]))
+			m_movement.y = 0;
+		if(Input.GetKeyUp(m_keys["backward"]))
+			m_movement.y = 0;
+		if(Input.GetKeyUp(m_keys["strafe_left"]))
+			m_movement.x = 0;
+		if(Input.GetKeyUp(m_keys["strafe_right"]))
+			m_movement.x = 0;
+
+		m_player.rigidbody.AddForce (new Vector3 (m_movement.x * m_normalSpeed, 0, m_movement.y * m_normalSpeed));
 	}
 }
